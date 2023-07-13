81f1b7607f51a75f6c6db54f8a745a417d8f46ef
unknown <ichiomafaustina@gmail..com>
2023-07-13 09:35:15 -0700

    meeeeeee

---
+++ src/data/data.js
@@ -0,0 +1,30 @@
+
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        github:"",
+        live:"",
+    },
+
+
+]
---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.css';
+import App from './App';
+
+import { createRoot } from 'react-dom';
+import App from './App';
+
+createRoot(document.getElementById('root')).render(<App />);
